2a35713f8eaf6339891980ae604e709ec0e387ef
Maciej <makoteq@gmail.com>
2025-05-17 16:23:21 +0200

    add tkinter GUI  :zap:

--- main.py
+++ main.py
@@ -1,66 +1,88 @@
-import numpy as np
-import matplotlib.pyplot as plt
+from euler import sim_Euler
+from tkinter import *
+from tkinter import messagebox
 
+# default values
 k = 10
 J = 0.5
 r = 2
 M = 1
 b = 0.1
 
-A = np.array([
-    [0, 1],
-    [k / ((J / (r**2)) - M), -b / (r**2 * ((J / (r**2)) - M))]
-])
-B = np.array([[0], [-1 / ((J / (r**2)) - M)]])
-
-x0 = np.array([0, 0]) # Starting values
+h = 0.0001 # discretization time constant
 tmax = 10
-h = 0.001 # Step size
 u_t = 1 # input signal
-steps = int(tmax / h) + 1
-x = np.zeros((steps, 2))
-x[0] = x0
 
 
-for n in range(steps - 1):
-    x[n + 1] = x[n] + h * (np.dot(A, x[n]) + B.flatten() * u_t)
+root = Tk()
+root.title("cart-modeling-simulation")
 
-interval = int(0.1 / h)
-for i in range(0, steps, interval):
-    print(f"{i * h:.1f}  {x[i,0]:.3f}  {x[i,1]:.3f}")
-    
-# Dislay last value
-print(f"{(steps-1) * h:.1f}  {x[(steps-1),0]:.3f}  {x[(steps-1),1]:.3f}")
+Label(root, text='Enter sim values:').grid(row=0, sticky=W, padx=10, pady=5)
 
+Label(root, text='Const k').grid(row=1, sticky=W, padx=10, pady=5)
+Label(root, text='Const J').grid(row=2, sticky=W, padx=10, pady=5)
+Label(root, text='Const r').grid(row=3, sticky=W, padx=10, pady=5)
+Label(root, text='Const M').grid(row=4, sticky=W, padx=10, pady=5)
+Label(root, text='Const b').grid(row=5, sticky=W, padx=10, pady=5)
 
+Label(root, text='Euler method specific values:').grid(row=6, sticky=W, padx=10, pady=5)
+Label(root, text='Discretization time const').grid(row=7, sticky=W, padx=10, pady=5)
+Label(root, text='Simulation time').grid(row=8, sticky=W, padx=10, pady=5)
 
+Label(root, text='Runge-Kutta method specific values:').grid(row=9, sticky=W, padx=10, pady=5)
+# TODO: Insert here Runge-Kutta values
 
+Label(root, text='Pick method:').grid(row=10, sticky=W, padx=10, pady=5)
 
+k_var = StringVar(value=str(k))
+J_var = StringVar(value=str(J))
+r_var = StringVar(value=str(r))
+M_var = StringVar(value=str(M))
+b_var = StringVar(value=str(b))
+h_var = StringVar(value="{:.4f}".format(h))
+tmax_var = StringVar(value=str(tmax))
 
+e_k = Entry(root, textvariable=k_var)
+e_J = Entry(root, textvariable=J_var)
+e_r = Entry(root, textvariable=r_var)
+e_M = Entry(root, textvariable=M_var)
+e_b = Entry(root, textvariable=b_var)
+e_h = Entry(root, textvariable=h_var)
+e_tmax = Entry(root, textvariable=tmax_var)
 
+e_k.grid(row=1, column=1, padx=10, pady=5)
+e_J.grid(row=2, column=1, padx=10, pady=5)
+e_r.grid(row=3, column=1, padx=10, pady=5)
+e_M.grid(row=4, column=1, padx=10, pady=5)
+e_b.grid(row=5, column=1, padx=10, pady=5)
+e_h.grid(row=7, column=1, padx=10, pady=5)
+e_tmax.grid(row=8, column=1, padx=10, pady=5)
 
+method = IntVar(value=0)
+euler_radio = Radiobutton(root, text="Euler", variable=method, value=0).grid(row=11, column=0, sticky=W, padx=10, pady=5)
+rk_radio = Radiobutton(root, text="Runge-Kutta", variable=method, value=1).grid(row=11, column=1, sticky=W, padx=10, pady=5)
 
+def start_sim():
+    try:
+        k = float(e_k.get())
+        J = float(e_J.get())
+        r = float(e_r.get())
+        M = float(e_M.get())
+        b = float(e_b.get())
+        h = float(e_h.get())
+        tmax = float(e_tmax.get())
+        
+        if method.get() == 0:
+            sim_Euler(k, J, r, M, b, h, tmax, u_t)
+        else:
+            # TODO : run Runge-Kutta method     
+            pass           
+    except ValueError:
+        messagebox.showerror("Error", "invalid input")
 
+start_button = Button(root, text="Start", command=start_sim, width=10)
+start_button.grid(row=12, column=0, columnspan=2, pady=10)
 
+root.mainloop()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# plt.plot([0, 1, 2], [0, 1, 4])
-# plt.title('Test plot')
-# plt.xlabel('x axis')
-# plt.ylabel('y axis')
-# plt.grid(True)
-# plt.show()
 
